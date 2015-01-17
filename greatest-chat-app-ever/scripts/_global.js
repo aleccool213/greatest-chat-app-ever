37cabb7c05a6dc2fd5e2fc423582d857c05476f2
Arjun Sarode <sarodearjun57@gmail.com>
2015-01-17 01:30:36 -0500

    add friendList

--- greatest-chat-app-ever/scripts/_global.js
+++ greatest-chat-app-ever/scripts/_global.js
@@ -1,19 +1,29 @@
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault("counter", 0);
+    // counter starts at 0
+    Session.setDefault("counter", 0);
 
-  //Test function to show all logged in users
-  Template.listOfOverallUsers.userInCollection = function(){
+    Template.listOfOverallUsers.userInCollection = function(){
         return Meteor.users.find();
     }
 
+    Template.friendList.helpers({
+        friends: function() {
+            var friendObjects = []
+            var friendIdArray = Meteor.userSettings.find({ type: 'friendList' });
+            for (i = 0; i < friendIdArray.length; i++) {
+                friendObjects.push(Meteor.users.find({ _id: friendIdArray[i] }));
+            }
 
+            return friendObjects
+        }
+    }); 
 }
 
 if (Meteor.isServer) {
-  Meteor.startup(function () {
+    Meteor.startup(function () {
     // code to run on server at startup
-  });
+
+    });
 
 
 }
