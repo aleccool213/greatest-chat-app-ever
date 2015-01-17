90cad1997647ed86cb67535ea42ee64d95f680fb
aleccool213 <moteygo@gmail.com>
2015-01-17 11:09:15 -0500

    FRIEND LIST WORKING GIRLS

--- greatest-chat-app-ever/global.js
+++ greatest-chat-app-ever/global.js
@@ -19,10 +19,12 @@ if (Meteor.isClient) {
     Template.friendList.helpers({
         friends: function() {
             var friendObjects = []
-            var friendIdArray = user_settings.find({ _id: Meteor.userId() }).fetch()
+            var friendIdArray = user_settings.find( { id: Meteor.userId() } ).fetch()[0].friendList;
+            console.log(friendIdArray);
 
             for (i = 0; i < friendIdArray.length; i++) {
-                friendObjects.push(Meteor.users.find({ _id: friendIdArray[i] }));
+                friendObjects.push(Meteor.users.findOne( friendIdArray[i] ));
+                console.log(Meteor.users.findOne( friendIdArray[i] ));
             }
 
             return friendObjects
@@ -86,20 +88,22 @@ if (Meteor.isClient) {
                 throw new Meteor.Error("not-authorized");
             }
 
-            var friendToAdd = Meteor.users.find({ 'services.twitter.screenName': friendScreenName }).fetch()
-            var friendToAddId = friendToAdd[0]._id
+            var friendToAdd = Meteor.users.find({ 'services.twitter.screenName': friendScreenName }).fetch();
+            var friendToAddId = friendToAdd[0]._id;
+            console.log("adding: "+ friendScreenName+ "with id: "+friendToAddId);
             // console.log(friendToAdd)
 
             if (friendToAdd.length == 1) {
 
                 if (user_settings.find({ id: Meteor.userId() }).fetch().length > 0) {
-                    user_settings.update({ id: Meteor.userId() }, { $addToSet: { friendList: friendToAddId }})
-                    console.log(user_settings.find().fetch())
+                    user_settings.update({ id: Meteor.userId() }, { $addToSet: { friendList: friendToAddId }});
+                    console.log(user_settings.find().fetch());
                 } else {
-                    user_settings.insert({ id: Meteor.userId(), friendList: [friendToAddId] })
-                    console.log(user_settings.find().fetch())
+                    user_settings.insert({ id: Meteor.userId(), friendList: [friendToAddId] });
+                    console.log(user_settings.find().fetch());
                 }
             }
+            
 
         },
 
