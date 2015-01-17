35f603c884d48d4e3883066c6feaee06bbe37f7c
Arjun Sarode <sarodearjun57@gmail.com>
2015-01-17 03:27:54 -0500

    Fix friendList and add chat-input

--- greatest-chat-app-ever/scripts/_global.js
+++ greatest-chat-app-ever/scripts/_global.js
@@ -9,7 +9,7 @@ if (Meteor.isClient) {
     Template.friendList.helpers({
         friends: function() {
             var friendObjects = []
-            var friendIdArray = Meteor.userSettings.find({ type: 'friendList' });
+            var friendIdArray = Meteor.userSettings.find({ _id: Meteor.userId }, { friendList: true });
             for (i = 0; i < friendIdArray.length; i++) {
                 friendObjects.push(Meteor.users.find({ _id: friendIdArray[i] }));
             }
@@ -17,6 +17,14 @@ if (Meteor.isClient) {
             return friendObjects
         }
     }); 
+
+    Template.chatRoom.events({
+        "keyup .chat-input" : function(event) {
+            if (event.which == 13 && !event.shiftKey) {
+                chatRoom.insert({ parent: this._id, dateCreated: new Date(), owner: Meteor.userId });
+            }
+        },
+    });
 }
 
 if (Meteor.isServer) {
